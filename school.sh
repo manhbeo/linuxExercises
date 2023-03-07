f50651c26818492c0ccea8f32b819c3e5ff11fc9
Manh Nguyen <mdnguyen4@wisc.edu>
2023-03-07 07:49:43 -0600

    fix bugs about cannot divide to float

--- school.sh
+++ school.sh
@@ -2,4 +2,16 @@
 cat "Property_Tax_Roll.csv" |
     grep "MADISON SCHOOLS"|
     cut -d ',' -f 7 |
-    { sum=0; count=0; while read n; do sum=$(($sum + $n)); count=$(($count + 1)); done; echo sum=$sum; echo average=$(($sum/$count)); }
+    { sum=0;
+      count=0;
+
+     while read n;
+      do sum=$(($sum + $n));
+	 count=$(($count + 1));
+      done;
+
+     echo sum=$sum;
+     bc <<EOF
+"average="
+EOF
+     echo "scale=6; $sum/ $count" | bc; }
